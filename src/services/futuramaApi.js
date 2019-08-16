0a9e22e241c069678137f91e2bb679eaa1038a1e
Jonathan Forney <jonathan.forney@gmail.com>
2019-08-16 16:41:19 -0700

    created api stuff

---
+++ src/services/futuramaApi.js
@@ -0,0 +1,19 @@
+export const getQuotes = (count = 10) => {
+  return fetch(`https://futuramaapi.herokuapp.com/api/quotes/${count}`)
+    .then(res => ([res.ok, res.json()]))
+    .then(([ok, json]) => {
+      if(!ok) throw 'Cant get there today pal';
+
+      return json;
+    });
+};
+
+export const getCharacterQuotes = (character, count = 10) => {
+  return fetch(`https://futuramaapi.herokuapp.com/api/character/${character}/${count}`)
+    .then(res => ([res.ok, res.json()]))
+    .then(([ok, json]) => {
+      if(!ok) throw 'I`m sorry Dave, I can`t do that right now...';
+
+      return json;
+    });
+};
