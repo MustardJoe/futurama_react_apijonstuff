dde6507f0d7385de507dbe1f91e874f37986ac2d
Jonathan Forney <jonathan.forney@gmail.com>
2019-08-17 17:10:46 -0700

    corrected error in url path for quotes by character

--- src/services/futuramaApi.js
+++ src/services/futuramaApi.js
@@ -9,7 +9,7 @@ export const getQuotes = (count = 10) => {
 };
 
 export const getCharacterQuotes = (character, count = 10) => {
-  return fetch(`https://futuramaapi.herokuapp.com/api/character/${character}/${count}`)
+  return fetch(`https://futuramaapi.herokuapp.com/api/characters/${character}/${count}`)
     .then(res => ([res.ok, res.json()]))
     .then(([ok, json]) => {
       if(!ok) throw 'I`m sorry Dave, I can`t do that right now...';
