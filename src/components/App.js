e45dca65fade39d556ced7452bf4c0686a324450
Jonathan Forney <jonathan.forney@gmail.com>
2019-08-17 17:08:00 -0700

    added quotes by character button to app js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 import TopQuotes from '../containers/quotes/TopQuotes';
+import CharQuotes from '../containers/quotes/CharQuotes';
 
 export default function App() {
   const [quotes, updateQuotes] = useState('top');
   
   return (
     <>
-      <h1>Hello World, go eat a b. of d.s...</h1>
+      <h1>Hello World, go eat a b. of bees...</h1>
+      <button onClick={() => updateQuotes('top')}>Top Quotes</button>
+      <button onClick={() => updateQuotes('character')}>Character Quotes</button>
       {quotes === 'top' && <TopQuotes />}
+      {quotes === 'character' && <CharQuotes />}
     </>
   );
 
